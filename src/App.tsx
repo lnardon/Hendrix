7f3d1fd52172b42cb340e1f7953ee30ce45a776d
lnardon <lucaslnardon@gmail.com>
2024-01-06 03:05:43 -0300

    ✨ Add base fretboard with major and minor scales

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,84 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from 'react';
+import './App.css';
+
+const notes = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
+const notesLength = notes.length;
+const scales = {
+    "Major": [0, 2, 4, 5, 7, 9, 11,12],
+    "Minor": [0,2,3,5,7,8,10,12]
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [tunning, setTunning] = useState(['E', 'B', 'G', 'D', 'A', 'E'])
+    const [rootScaleNote, setRootScaleNote] = useState("C")
+    const [scaleType, setScaleType] = useState("Major")
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    const getScaleNotes = (root, scaleType) => {
+        const rootIndex = notes.indexOf(root);
+        return scales[scaleType].map(interval => notes[(rootIndex + interval) % notesLength]);
+    };
+
+    const renderFretboard = () => {
+        const scaleNotes = getScaleNotes(rootScaleNote, scaleType);
+        return tunning.map((stringNote) => {
+            const startIndex = notes.indexOf(stringNote);
+            const frets = [];
+            for (let i = 1; i <= notesLength; i++) {
+                const noteIndex = (startIndex + i) % notesLength;
+                const note = notes[noteIndex];
+                frets.push(
+                    <div key={i} className="fret" style={{ animationDelay: i * 32 + "ms", filter: `opacity(${scaleNotes.includes(note) ? "100%" : "16%"})` }}>
+                        {note}
+                    </div>
+                );
+            }
+
+            return (
+                <div key={stringNote} className="string">
+                    {frets}
+                </div>
+            );
+        });
+    };
+
+    useEffect(() => renderFretboard, [tunning, rootScaleNote, scaleType]);
+
+    return (
+        <div className="App">
+            <h1>Fretboard</h1>
+            <div>
+                <select className="scale-select" onChange={(e) => setRootScaleNote(e.target.value)} value={rootScaleNote}>
+                    {notes.map(note => (
+                        <option key={note} value={note}>{note}</option>
+                    ))}
+                </select>
+                <select className="scale-select" onChange={(e) => setScaleType(e.target.value)} value={scaleType}>
+                    {Object.keys(scales).map(type => (
+                        <option key={type} value={type}>{type}</option>
+                    ))}
+                </select>
+            </div>
+            <div className="fretboard">
+                <div className="tuner">
+                    {tunning.map((tune, index) => (
+                        <select className="tunner-select" key={index} onChange={e => {
+                            let newTuning = [...tunning];
+                            newTuning[index] = e.target.value;
+                            setTunning(newTuning);
+                        }} value={tune}>
+                            {notes.map(note => (
+                                <option key={note} value={note}>{note}</option>
+                            ))}
+                        </select>
+                    ))}
+                </div>
+                <div>
+                    {renderFretboard()}
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
+
